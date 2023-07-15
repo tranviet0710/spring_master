4f58e34bf617fa9edec508528aea2e3bcf6dc52c
tranviet0710 <102190298@sv1.dut.udn.vn>
2023-07-15 14:19:29 +0700

    eazy school webpage

---
+++ Example7/src/main/java/com/eazybytes/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.eazybytes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author VietDev
+ * @date 15/07/2023
+ * @role
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/courses").setViewName("courses");
+    }
+}
+
---
+++ Example7/src/main/java/com/eazybytes/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.eazybytes.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author VietDev
+ * @date 14/07/2023
+ * @role
+ */
+@Controller
+public class HomeController {
+    @RequestMapping(value={"","/","home"})
+    public String displayHomepage(){
+        return "home.html";
+    }
+
+}
+
