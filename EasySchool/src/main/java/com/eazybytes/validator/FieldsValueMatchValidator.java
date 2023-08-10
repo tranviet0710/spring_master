a2a60d3c3a673ecd23ce074bb76f9c1c858bc3d0
tranviet0710 <102190298@sv1.dut.udn.vn>
2023-08-11 00:02:14 +0700

    spring.jpa.properties.javax.persistence.validation.mode = none

--- EasySchool/src/main/java/com/eazybytes/validator/FieldsValueMatchValidator.java
+++ EasySchool/src/main/java/com/eazybytes/validator/FieldsValueMatchValidator.java
@@ -26,12 +26,17 @@ public class FieldsValueMatchValidator implements ConstraintValidator<FieldsValu
                 .getPropertyValue(field);
         Object fieldMatchValue = new BeanWrapperImpl(value)
                 .getPropertyValue(fieldMatch);
+//        if (fieldValue != null) {
+//            if (fieldValue.toString().startsWith("$2a")) {
+//                return true;
+//            } else {
+//                return fieldValue.equals(fieldMatchValue);
+//            }
+//        } else {
+//            return fieldMatchValue == null;
+//        }
         if (fieldValue != null) {
-            if (fieldValue.toString().startsWith("$2a")) {
-                return true;
-            } else {
-                return fieldValue.equals(fieldMatchValue);
-            }
+            return fieldValue.equals(fieldMatchValue);
         } else {
             return fieldMatchValue == null;
         }
