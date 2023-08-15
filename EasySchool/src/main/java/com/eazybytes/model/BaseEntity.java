6b0a0024d5ebc7cb04649a7f3d56b9c96e7b5596
tranviet0710 <102190298@sv1.dut.udn.vn>
2023-08-15 22:43:36 +0700

    content filter with @JsonIgnore

--- EasySchool/src/main/java/com/eazybytes/model/BaseEntity.java
+++ EasySchool/src/main/java/com/eazybytes/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.eazybytes.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -23,18 +24,22 @@ import java.time.LocalDateTime;
 public class BaseEntity {
     @CreatedDate
     @Column(updatable = false)
+    @JsonIgnore
     private LocalDateTime createdAt;
 
     @CreatedBy
     @Column(updatable = false)
+    @JsonIgnore
     private String createdBy;
 
     @LastModifiedDate
     @Column(insertable = false)
+    @JsonIgnore
     private LocalDateTime updatedAt;
 
     @LastModifiedBy
     @Column(insertable = false)
+    @JsonIgnore
     private String updatedBy;
 }
 
--- EasySchool/src/main/java/com/eazybytes/rest/ContactRestController.java
+++ EasySchool/src/main/java/com/eazybytes/rest/ContactRestController.java
@@ -1,11 +1,11 @@
 package com.eazybytes.rest;
 
-import com.eazybytes.model.Contact;
 import com.eazybytes.model.Response;
 import com.eazybytes.services.ContactService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +20,14 @@ import java.util.List;
  */
 @RestController
 @RequestMapping(value = "/api/v1", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+@CrossOrigin("*")
 public class ContactRestController {
     private ContactService contactService;
+
     @GetMapping("/responses")
-    public ResponseEntity<Object> getAllResponse(){
+    public ResponseEntity<Object> getAllResponse() {
         List<Response> responses = Arrays.asList(
-          new Response("400", "BAD REQUEST"),
+                new Response("400", "BAD REQUEST"),
                 new Response("201", "CREATED"),
                 new Response("200", "OK"),
                 new Response("500", "SERVER ERROR")
