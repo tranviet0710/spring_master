9d66943796df0c15c19b05aa42b82a8400a20b1c
tranviet0710 <102190298@sv1.dut.udn.vn>
2023-08-16 12:30:58 +0700

    consuming rest api using rest template and web client

--- APIConsuming/src/main/java/com/example/api_consuming/configuration/ProjectConfiguration.java
+++ APIConsuming/src/main/java/com/example/api_consuming/configuration/ProjectConfiguration.java
@@ -1,19 +1,39 @@
 package com.example.api_consuming.configuration;
 
 import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * @author VietDev
  * @date 16/08/2023
  * @role
  */
+@Configuration
 public class ProjectConfiguration {
     private static final String username = "admin@gmail.com";
     private static final String password = "password123";
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
         return new BasicAuthRequestInterceptor(username, password);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        return restTemplateBuilder.basicAuthentication(username, password).build();
+    }
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder()
+                .filter(ExchangeFilterFunctions.basicAuthentication(username, password))
+                .build();
     }
 }
 
--- APIConsuming/src/main/java/com/example/api_consuming/controller/ResponseController.java
+++ APIConsuming/src/main/java/com/example/api_consuming/controller/ResponseController.java
@@ -3,10 +3,17 @@ package com.example.api_consuming.controller;
 import com.example.api_consuming.model.Response;
 import com.example.api_consuming.proxy.ContactProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import javax.print.attribute.standard.Media;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,9 +26,31 @@ public class ResponseController {
     @Autowired
     ContactProxy contactProxy;
 
-    @GetMapping("/responses")
-    public List<Response> getResponses(@RequestParam("status") String status){
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    WebClient webClient;
+    @GetMapping("/feign_client/responses")
+    public List<Response> getFeignClientResponses(@RequestParam("status") String status){
         return contactProxy.getAllResponse(status);
+    }
+
+    @GetMapping("/rest_template/responses")
+    public ResponseEntity<Response[]> getRestTemplateResponses(@RequestParam("status") String status){
+        String uri = "http://localhost:8080/api/v1/responses";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+        ResponseEntity<Response[]> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, Response[].class);
+        return responseEntity;
+    }
+
+    @GetMapping("/web_client/responses")
+    public Flux<Response> getWebClientResponses(@RequestParam("status") String status){
+        String uri = "http://localhost:8080/api/v1/responses";
+        return webClient.get().uri(uri).retrieve().bodyToFlux(Response.class);
     }
 }
 
